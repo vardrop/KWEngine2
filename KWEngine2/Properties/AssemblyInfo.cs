58964ac09ace7d7512910c5cbbcb51088793e87b
Karau Lutz <lutz.karau@gmail.com>
2020-03-04 15:06:00 +0100

    Increased version number.

--- KWEngine2/Properties/AssemblyInfo.cs
+++ KWEngine2/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("C# 3D Gaming Engine")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lutz Karau, Markus Wienhöfer")]
-[assembly: AssemblyProduct("KWEngine2 (Updated on: 2020-03-03)")]
+[assembly: AssemblyProduct("KWEngine2 (Updated on: 2020-03-04)")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
